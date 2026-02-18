969d43300b0d65a3ebc36e96d64499754239bb2e
ynishimi <nishimi.yuki.37z@st.kyoto-u.ac.jp>
2026-02-18 15:57:52 +0100

    impl simple benchmark

--- distributor/node_test.go
+++ distributor/node_test.go
@@ -10,6 +10,45 @@ import (
 	"github.com/ynishimi/distributed-llm-dissemination/distributor"
 )
 
+func distribute(t testing.TB, transports []distributor.Transport, assignment distributor.Assignment, layers distributor.Layers, LeaderNodeID distributor.NodeID, NumReceivers int) {
+	transCounter := 0
+
+	// leader
+	n := distributor.NewNode(LeaderNodeID, LeaderNodeID, transports[transCounter])
+	transCounter++
+	leader := distributor.NewLeaderNode(n, layers, assignment)
+
+	// receivers
+	receivers := make([]*distributor.ReceiverNode, NumReceivers)
+	for i := range NumReceivers {
+		receiverTransport := transports[transCounter]
+		transCounter++
+		receiver := distributor.NewReceiverNode(distributor.NewNode(distributor.NodeID(int(LeaderNodeID)+i+1), LeaderNodeID, receiverTransport), make(distributor.Layers))
+		receivers[i] = receiver
+
+		// receivers announce its existence to the leader
+		err := receiver.Announce()
+		require.NoError(t, err)
+	}
+
+	// leader should start sending layers after collecting annoucements from receivers
+	select {
+	case msg := <-leader.StartDistribution():
+		fmt.Print(msg)
+	case <-time.After(time.Second):
+		t.Fatal("timeout waiting for announcements from receivers")
+	}
+
+	// leader should send layers; wait for the leader to collect acks from receivers
+	select {
+	case ready := <-leader.Ready():
+		require.Equal(t, ready, assignment)
+
+	case <-time.After(time.Second):
+		t.Fatal("timeout waiting for Ready()")
+	}
+}
+
 func TestSimpleDistribution(t *testing.T) {
 	// assignment and layers
 	const NumLayers = 3
@@ -35,46 +74,6 @@ func TestSimpleDistribution(t *testing.T) {
 		assignment[distributor.NodeID(i+LeaderNodeID+1)] = layerIDs
 	}
 
-	distribute := func(t *testing.T, transports []distributor.Transport) {
-		t.Helper()
-		transCounter := 0
-
-		// leader
-		n := distributor.NewNode(LeaderNodeID, LeaderNodeID, transports[transCounter])
-		transCounter++
-		leader := distributor.NewLeaderNode(n, layers, assignment)
-
-		// receivers
-		receivers := make([]*distributor.ReceiverNode, NumReceivers)
-		for i := range NumReceivers {
-			receiverTransport := transports[transCounter]
-			transCounter++
-			receiver := distributor.NewReceiverNode(distributor.NewNode(distributor.NodeID(LeaderNodeID+i+1), LeaderNodeID, receiverTransport), make(distributor.Layers))
-			receivers[i] = receiver
-
-			// receivers announce its existence to the leader
-			err := receiver.Announce()
-			require.NoError(t, err)
-		}
-
-		// leader should start sending layers after collecting annoucements from receivers
-		select {
-		case msg := <-leader.StartDistribution():
-			fmt.Print(msg)
-		case <-time.After(time.Second):
-			t.Fatal("timeout waiting for announcements from receivers")
-		}
-
-		// leader should send layers; wait for the leader to collect acks from receivers
-		select {
-		case ready := <-leader.Ready():
-			require.Equal(t, ready, assignment)
-
-		case <-time.After(time.Second):
-			t.Fatal("timeout waiting for Ready()")
-		}
-	}
-
 	t.Run("inmem", func(t *testing.T) {
 
 		transports := make([]distributor.Transport, NumPeers)
@@ -82,7 +81,7 @@ func TestSimpleDistribution(t *testing.T) {
 			transports[i] = distributor.NewInmemTransport(fmt.Sprint(LeaderNodeID+i), NumPeers)
 		}
 
-		distribute(t, transports)
+		distribute(t, transports, assignment, layers, LeaderNodeID, NumReceivers)
 	})
 	t.Run("tcp", func(t *testing.T) {
 		addrs := make(distributor.AddrRegistory, NumPeers)
@@ -99,6 +98,50 @@ func TestSimpleDistribution(t *testing.T) {
 			}
 		})
 
-		distribute(t, transports)
+		distribute(t, transports, assignment, layers, LeaderNodeID, NumReceivers)
 	})
+}
+
+func BenchmarkSimpleDistributionTcp(b *testing.B) {
+	// assignment and layers
+	const NumLayers = 3
+	const NumReceivers = 3
+	const NumPeers = NumReceivers + 1
+	const LeaderNodeID = 0
+	// layers
+	layers := make(distributor.Layers)
+	for i := range NumReceivers {
+		// add dummy data as small random Bytes
+		randBytes := make([]byte, 1)
+		rand.Read(randBytes)
+		layer := distributor.Layer(randBytes)
+
+		layers[distributor.LayerID(i+LeaderNodeID+1)] = &layer
+	}
+
+	// assinment
+	assignment := make(distributor.Assignment)
+	for i := range NumReceivers {
+		layerIDs := make(distributor.LayerIDs)
+		// assign layer i to node i this time
+		layerIDs[distributor.LayerID(i+LeaderNodeID+1)] = struct{}{}
+		assignment[distributor.NodeID(i+LeaderNodeID+1)] = layerIDs
+	}
+
+	for b.Loop() {
+		// TCP transport
+		addrs := make(distributor.AddrRegistory, NumPeers)
+		transports := make([]distributor.Transport, NumPeers)
+		for i := range NumPeers {
+			addrs[distributor.NodeID(i)] = fmt.Sprintf(":%d", 8080+LeaderNodeID+i)
+		}
+		for i := range NumPeers {
+			transports[i] = distributor.NewTcpTransport(addrs[distributor.NodeID(i)], NumPeers, addrs)
+		}
+		distribute(b, transports, assignment, layers, LeaderNodeID, NumReceivers)
+
+		for _, tr := range transports {
+			tr.Close()
+		}
+	}
 }
