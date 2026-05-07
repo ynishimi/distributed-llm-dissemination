e3969568e4139d65fb74aab40507d4e499b7670f
ynishimi <nishimi.yuki.37z@st.kyoto-u.ac.jp>
2026-05-07 21:24:41 +0200

    correct layer and disk assignment logic for crash scenarios

--- solver.py
+++ solver.py
@@ -3,27 +3,35 @@ import numpy as np
 
 # problem data
 
-LAYERSIZE = 1.81 * 2**30
-NETWORKBW = 12.5/8 * 10**9
-DISKBW = 200 * 2**30
+LAYERSIZE = 1.81 * 2**30  # 1.81 GiB
+DISKSIZE = 20 * 2**30  # 20 GiB
+NETWORKBW = 12.5/8 * 10**9  # 12.5 Gbps
+DISKBW = 200 * 2**20  # 200 MiB
 
 N = 4
 '''num of nodes'''
 L = 36
 '''num of layers'''
-P = np.array([0.5 for _ in range(N)])
+P = np.array([1/N for _ in range(N)])
+# P = np.array([0.2, 0.6, 0.1, 0.1])
 '''probability of crash'''
 S = np.array([LAYERSIZE for _ in range(L)])
 '''size of layers (= |l|)'''
-C = np.array([LAYERSIZE * L / (N - 1) for _ in range(N)]
+C = np.array([DISKSIZE for _ in range(N)]
              )  # this time, setting the capacity to minimum.
 '''size of disks'''
+
 B = np.array([NETWORKBW for _ in range(N)])
 '''network bandwidth of nodes'''
-
 D = np.array([DISKBW for _ in range(N)])
 '''rate of disks'''
 
+INIT_ASSIGNMENT = [[L//N * i + j for j in range(L//N)]
+                   for i in range(N)]
+DUMMY_ASSIGNMENT = [[L//(N-1) * i + j for j in range(L//(N-1))]
+                    for i in range(N-1)]
+ALT_ASSIGNMENTS = [DUMMY_ASSIGNMENT[:i] + [[]] +
+                   DUMMY_ASSIGNMENT[i:] for i in range(N)]
 
 # variables
 
@@ -31,39 +39,64 @@ D = np.array([DISKBW for _ in range(N)])
 x = cp.Variable((N, L), nonneg=True)
 t = cp.Variable(N, nonneg=True)  # downtime for each node's crash scenario
 f = {}  # edges in the flow graph
-for k in range(N):  # for each crash scenario:
-    f[k] = {}
-    f[k]['src_sender'] = cp.Variable(N, nonneg=True)
-    f[k]['sender_disk'] = cp.Variable(N, nonneg=True)
-    f[k]['disk_layer'] = cp.Variable((N, L), nonneg=True)
-    f[k]['layer_receiver'] = cp.Variable(L, nonneg=True)
-    f[k]['receiver_sink'] = cp.Variable(nonneg=True)
+for crashed_node in range(N):  # for each crash scenario:
+    f[crashed_node] = {}
+    f[crashed_node]['src_sender'] = cp.Variable(N, nonneg=True)
+    f[crashed_node]['sender_disk'] = cp.Variable(
+        N, nonneg=True)  # TODO: add a client?
+    f[crashed_node]['disk_layer'] = cp.Variable((N, L), nonneg=True)
+    f[crashed_node]['layer_receiver'] = cp.Variable(L, nonneg=True)
+    f[crashed_node]['receiver_sink'] = cp.Variable(N, nonneg=True)
 
 # constraints
 
 constraints = []
-
 constraints.append(x <= 1)
 constraints.append(x @ S <= C)  # disk capacity constraint
 
-for k in range(N):
-    constraints.append(f[k]['src_sender'] <= cp.multiply(B, t[k]))
-    constraints.append(f[k]['src_sender'] == f[k]['sender_disk'])
-    constraints.append(f[k]['sender_disk'] <= cp.multiply(D, t[k]))
-    constraints.append(f[k]['sender_disk'] ==
-                       cp.sum(f[k]['disk_layer'], axis=1))
-    constraints.append(f[k]['disk_layer'] <= cp.multiply(x, S))
+for crashed_node in range(N):
+    # network bw
+    constraints.append(f[crashed_node]['src_sender'] <=
+                       cp.multiply(B, t[crashed_node]))
+    # flow
+    constraints.append(f[crashed_node]['src_sender'] ==
+                       f[crashed_node]['sender_disk'])
+    # disk bw
+    constraints.append(f[crashed_node]['sender_disk'] <=
+                       cp.multiply(D, t[crashed_node]))
+    # flow
+    constraints.append(f[crashed_node]['sender_disk'] ==
+                       cp.sum(f[crashed_node]['disk_layer'], axis=1))
+    # data obtained by each disk
+    constraints.append(f[crashed_node]['disk_layer'] <= cp.multiply(x, S))
+    # flow
+    for node_asgn in range(len(ALT_ASSIGNMENTS[crashed_node])):
+        if len(ALT_ASSIGNMENTS[crashed_node][node_asgn]) == 0:
+            continue
+        constraints.append(cp.sum(f[crashed_node]['disk_layer'][:, ALT_ASSIGNMENTS[crashed_node][node_asgn]],
+                           axis=0) == f[crashed_node]['layer_receiver'][ALT_ASSIGNMENTS[crashed_node][node_asgn]])
 
-    # TODO: refer to a newer layer assignment
+    # missing layers should be sent, but existing layers should not be sent or saved
+    for receiver in range(N):
+        for l in ALT_ASSIGNMENTS[crashed_node][receiver]:
+            if l in INIT_ASSIGNMENT[receiver]:
+                constraints.append(f[crashed_node]['layer_receiver'][l] == 0)
+                constraints.append(x[receiver, l] == 0)
+            else:
+                constraints.append(
+                    f[crashed_node]['layer_receiver'][l] == S[l])
+
+    # flow
     constraints.append(
-        cp.sum(f[k]['disk_layer'], axis=0) == f[k]['layer_receiver'])
-
-    constraints.append(f[k]['layer_receiver'] == S)
-    constraints.append(cp.sum(f[k]['layer_receiver']) == f[k]['receiver_sink'])
-    constraints.append(f[k]['receiver_sink'] <= B[k] * t[k])
-
+        cp.sum(f[crashed_node]['layer_receiver']) == f[crashed_node]['receiver_sink'])
+    # network bw
+    constraints.append(f[crashed_node]['receiver_sink']
+                       <= cp.multiply(B, t[crashed_node]))
+    # network bandwidth should be shared among the same node (sender k and receiver k)
+    constraints.append(f[crashed_node]['src_sender'] + f[crashed_node]
+                       ['receiver_sink'] <= cp.multiply(B, t[crashed_node]))
     # crashed node doesn't send layers
-    constraints.append(f[k]['src_sender'][k] == 0)
+    constraints.append(f[crashed_node]['src_sender'][crashed_node] == 0)
 
 # objective function
 obj = cp.Minimize(P @ t)
